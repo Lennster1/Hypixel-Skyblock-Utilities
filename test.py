743758e7420d995e9b936463beef5da9a4613884
Lennster1 <lennyluo0709@gmail.com>
2021-06-23 08:48:32 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,15 @@
 import requests
 import json
-from time import sleep
+from dotenv import load_dotenv
+import os
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
 
 
-
-
-response = requests.get("https://api.hypixel.net/skyblock/bazaar")
+response = requests.get("https://api.hypixel.net/skyblock/bazaar/products?key=" + API_KEY)
 if response.status_code != 200:
-    print("There was an error connecting to the server! Try again later")
-    sleep(5)
+    print("There was an error connecting to the server. Check that your API KEY is correct. If it is, its probably a hypixel problem")
+    print("Try again later.")
     exit()
 else:
     print("Connection succesful!")
