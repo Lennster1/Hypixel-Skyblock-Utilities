a864c861989cef53988d5f02015aa828feda3925
outsparkled <83491180+outsparkled@users.noreply.github.com>
2021-06-23 21:36:12 +0200

    Update bazaar_prices.py

--- bazaar_prices.py
+++ bazaar_prices.py
@@ -1,27 +1,43 @@
 import requests
 import json
 from time import sleep
-keepGoing = "y"
+
 response = requests.get("https://api.hypixel.net/skyblock/bazaar")
-if response.status_code != 200:   
-    print("There was an error connecting to the server.")
-    sleep(2)
-    print("Try again later.")
-    exit()
-else:
-    print("Connecting to the server... ")
-    sleep(2)
-    print("Connection succesful!")
-    sleep(1)
-    print("Getting data...")
-    sleep(2)
-    print("Data successfully recived!")
-    sleep(1)
+sendConnectionInfo(response)
+
+keepGoing = "y"
+
 while keepGoing == "y": 
     requestedProduct = input ("What product would you like to search for? Please note that multi-worded items require an underscore between spaces. | e.g; enchanted_lava_bucket | ")
     trueRequestedProduct = requestedProduct.upper()
-    print (trueRequestedProduct)
-    data = requests.get("https://api.hypixel.net/skyblock/bazaar").json()
+    print(trueRequestedProduct)
+    
+    getAndOutputPrice(response, trueRequestedProduct)
+    
+    keepGoing = input ("Would you like to search for another product? (y/n) ")
+    if keepGoing == "n":
+        break
+        print("Thank you for using Hypixel-Skyblock-Utilities by Lennster1")
+
+
+def sendConnectionInfo(response):
+    if response.status_code != 200:   
+        print("There was an error connecting to the server.")
+        sleep(2)
+        print("Try again later.")
+        exit()
+    else:
+        print("Connecting to the server... ")
+        sleep(2)
+        print("Connection succesful!")
+        sleep(1)
+        print("Getting data...")
+        sleep(2)
+        print("Data successfully recived!")
+        sleep(1)
+        
+def getAndOutputPrice(response, trueRequestedProduct):
+    data = response.json()
     try: 
         product_sellPrice = data["products"][trueRequestedProduct]["sell_summary"][0]["pricePerUnit"]
         product_buyPrice = data["products"][trueRequestedProduct]["buy_summary"][0]["pricePerUnit"]
@@ -30,10 +46,5 @@ while keepGoing == "y":
     except KeyError:
         pass
         print("Hey! That item doesnt exist!")
-    keepGoing = input ("Would you like to search for another product? (y/n) ")
-    if keepGoing == "n":
-        break
-print("Thank you for using Hypixel-Skyblock-Utilities by Lennster1")
-
 
 
